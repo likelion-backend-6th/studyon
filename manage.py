0009a2124f3e32b75c32ddcecf6df197ab16aaf2
25th-Night <brown_eyed87@naver.com>
2023-09-27 21:19:59 +0900

    Fix) manage: 기본값을 환경변수에서 가져오도록 수정

--- manage.py
+++ manage.py
@@ -4,9 +4,12 @@ import os
 import sys
 
 
+ENV = os.getenv("RUN_MODE", "base")
+
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{ENV}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
