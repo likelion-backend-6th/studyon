53caea5494799136b6c30736dfb381cca7c6524c
Lee816 <lgha0816@naver.com>
2023-09-27 16:50:54 +0900

    Update) 스터디 관리 페이지 - 출력목록수정
    내가 생성한 모집글 목록
    내가 찜한 모집글 목록
    내가 팀장인 스터디 목록
    내가 팀원인 스터디 목록

--- manager/views.py
+++ manager/views.py
@@ -2,6 +2,7 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.contrib.auth.models import User
+from recruit.models import Recruit
 from .models import Study
 
 class StudyView(LoginRequiredMixin,generic.ListView):
@@ -11,6 +12,8 @@ class StudyView(LoginRequiredMixin,generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['my_studies'] = Study.objects.filter(creator=self.request.user)
-        context['in_studies'] = Study.objects.filter(members=self.request.user)
+        context['in_studies'] = Study.objects.filter(members=self.request.user).exclude(creator=self.request.user)
+        context['my_recruits'] = Recruit.objects.filter(creator=self.request.user)
+        context['like_recruits'] = Recruit.objects.filter(like_users=self.request.user)
         return context
     
