3040b5d254b9304abd64988fc10b6b2a455f3b55
Lee816 <lgha0816@naver.com>
2023-09-28 19:40:21 +0900

    Feat) 스터디 완료,종료 버튼 기능 추가
    + 스터디 status 값 출력 수정

--- manager/urls.py
+++ manager/urls.py
@@ -6,7 +6,9 @@ from . import views
 app_name = "manager"
 
 urlpatterns = [
-    path("", views.StudyView.as_view(), name="stuydies_list"),
+    path("", views.StudyView.as_view(), name="studies_list"),
     path("<int:pk>/", views.StudyDetailView.as_view(), name="study_detail"),
+    path("<int:pk>/done", views.StudyDoneView.as_view(), name="study_done"),
+    path("<int:pk>/finished", views.StudyDoneView.as_view(), name="study_finished"),
     path("tasks/<int:pk>/", views.PostView.as_view(), name="post_list"),
 ]
