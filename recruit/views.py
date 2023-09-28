377c3f1c9a21df163da2c3ba9e0ce43745e33375
Jeong Taek <imjt9345@naver.com>
2023-09-28 15:41:41 +0900

    Feat) 메인페이지-버튼 로그인,로그아웃,스터디 생성, 스터디 관리 Update) 폴더이름 변경 recruit->recruits

--- recruit/views.py
+++ recruit/views.py
@@ -6,15 +6,19 @@ from recruit.models import Recruit
 # Create your views here.
 class RecruitView(ListView):
     model = Recruit
-    template_name = 'recruit/index.html'
-    context_object_name = 'recruits'
+    template_name = "recruits/index.html"
+    context_object_name = "recruits"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
-            context['in_studies'] = Recruit.objects.filter(study__members=self.request.user)
-            context['liked_studies'] = Recruit.objects.filter(like_users=self.request.user)
+            context["in_studies"] = Recruit.objects.filter(
+                study__members=self.request.user
+            )
+            context["liked_studies"] = Recruit.objects.filter(
+                like_users=self.request.user
+            )
         else:
-            context['in_studies'] = None
-            context['liked_studies'] = None
+            context["in_studies"] = None
+            context["liked_studies"] = None
         return context
