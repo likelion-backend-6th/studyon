34550d1473124d1aa96257ee62eb2e6efaad01cb
25th-Night <brown_eyed87@naver.com>
2023-09-28 17:28:19 +0900

    Feat) manager/urls: 생성한 뷰의 url 등록

--- manager/urls.py
+++ manager/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+
 app_name = "manager"
+
 urlpatterns = [
     path("", views.StudyView.as_view(), name="stuydies_list"),
     path("<int:pk>/", views.StudyDetailView.as_view(), name="stuydy_detail"),
+    path("tasks/<int:pk>/", views.PostView.as_view(), name="post_list"),
 ]
