d2db52619f8575acda64f391270c491ff7f41c4c
25th-Night <brown_eyed87@naver.com>
2023-09-28 17:27:23 +0900

    Feat) base: STATICFILES_DIRS 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -133,6 +133,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = "/var/www/html/static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
