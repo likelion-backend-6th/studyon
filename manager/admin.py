3bc1cc75c978a36c3765ec9dc862181722d62a12
25th-Night <brown_eyed87@naver.com>
2023-09-28 23:14:34 +0900

    Feat) manager/admin: MarkdownxModelAdmin 적용

--- manager/admin.py
+++ manager/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+
+from markdownx.admin import MarkdownxModelAdmin
+
 from .models import Study, Task, Post, File
+
 
 admin.site.register(Study)
 admin.site.register(Task)
-admin.site.register(Post)
+admin.site.register(Post, MarkdownxModelAdmin)
 admin.site.register(File)
