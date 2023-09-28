4d206be4ec5fc9821ec9aa8c6ee81c670b8468a3
Jeong Taek <imjt9345@naver.com>
2023-09-28 20:00:31 +0900

    Feat) 메인페이지 찜하기 기능

--- recruit/urls.py
+++ recruit/urls.py
@@ -7,4 +7,6 @@ app_name = "recruits"
 
 urlpatterns = [
     path("", views.RecruitView.as_view(), name="index"),
+    path("recruit/<int:pk>/like/", views.like_recruit, name="like_recruit"),
+    path("recruit/<int:pk>/unlike/", views.unlike_recruit, name="unlike_recruit"),
 ]
