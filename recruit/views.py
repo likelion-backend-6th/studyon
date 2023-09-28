9e960d84ef805eb6a10f526a0bdab81d6f1e393e
Jeong Taek <imjt9345@naver.com>
2023-09-28 17:45:43 +0900

    Feat) title, tag 검색 기능

--- recruit/views.py
+++ recruit/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 
+from recruit.forms import SearchForm
 from recruit.models import Recruit
 
 
@@ -9,8 +10,22 @@ class RecruitView(ListView):
     template_name = "recruits/index.html"
     context_object_name = "recruits"
 
+    def get_queryset(self):
+        queryset = Recruit.objects.all()
+        query = self.request.GET.get("query")
+        tag = self.request.GET.get("tag")
+
+        if query:
+            queryset = queryset.filter(title__icontains=query)
+
+        if tag:
+            queryset = queryset.filter(tags__name__icontains=tag)
+
+        return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["form"] = SearchForm()
         if self.request.user.is_authenticated:
             context["in_studies"] = Recruit.objects.filter(
                 study__members=self.request.user
