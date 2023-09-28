793f6e8752775fd1c1dfd89f9d59515587e336db
Jeong Taek <imjt9345@naver.com>
2023-09-28 15:57:38 +0900

    Update) 오타 수정 stuydy->study

--- manager/urls.py
+++ manager/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name='manager'
 urlpatterns = [
-    path('', views.StudyView.as_view(), name='stuydies_list'),
-    path('<int:pk>/', views.StudyDetailView.as_view(), name='stuydy_detail'),
+    path('', views.StudyView.as_view(), name='studies_list'),
+    path('<int:pk>/', views.StudyDetailView.as_view(), name='study_detail'),
 ]
