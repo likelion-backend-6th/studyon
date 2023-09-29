202c477365740ca4ba333c7714dd868a8260d7a0
Jeong Taek <imjt9345@naver.com>
2023-09-29 17:38:19 +0900

    Feat) 스터디 모집 상세 페이지

--- recruit/urls.py
+++ recruit/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from recruit import views
 
-
 app_name = "recruits"
 
 urlpatterns = [
     path("", views.RecruitView.as_view(), name="index"),
     path("recruit/<int:pk>/like/", views.like_recruit, name="like_recruit"),
     path("recruit/<int:pk>/unlike/", views.unlike_recruit, name="unlike_recruit"),
+    path(
+        "recruits/<int:pk>/", views.RecruitDetailView.as_view(), name="recruit_detail"
+    ),
 ]
