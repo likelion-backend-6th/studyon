8e7843bc0e4dc54bc3dcafc8d8ec3cd57300f208
terry <munu220@gmail.com>
2023-09-30 12:36:04 +0000

    Feat) 유저 리뷰 확인 추가

--- user/views.py
+++ user/views.py
@@ -1,7 +1,12 @@
+from typing import Any
+from django.db import models
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.views.generic import View, ListView
 from django.contrib.auth.models import User
 from django.contrib import auth
+
+from .models import Review
 
 
 def logout(request):
@@ -55,3 +60,24 @@ class SignupView(View):
                 self.template_name,
                 {"error": "password and password_check is not same"},
             )
+
+
+class MyHistoryView(ListView):
+    model = Review
+    template_name = "users/info.html"
+    context_object_name = "reviews"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["my_reviews"] = Review.objects.filter(reviewer=self.request.user)
+        print(context)
+        return context
+
+    def get_queryset(self):
+        user = self.request.user
+        return super().get_queryset().filter(reviewee=user)
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if request.user.is_authenticated:
+            return super().get(request, *args, **kwargs)
+        return redirect("users:login")
