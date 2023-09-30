2d664ad86a168893bfe9195ee77d7a23d40803d2
25th-Night <brown_eyed87@naver.com>
2023-09-30 19:17:29 +0900

    common/utils: s3_file_upload 기능 구현

---
+++ common/utils.py
@@ -0,0 +1,43 @@
+import uuid
+from datetime import datetime
+
+from django.conf import settings
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from boto3 import client
+
+
+def s3_file_upload(upload_file: InMemoryUploadedFile, parent_directory):
+    service_name = "s3"
+    aws_access_key_id = settings.NCP_S3_ACCESS_KEY
+    aws_secret_access_key = settings.NCP_S3_SECRET_KEY
+    endpoint_url = settings.NCP_S3_ENDPOINT_URL
+    bucket_name = settings.NCP_S3_BUCKET_NAME
+
+    s3 = client(
+        service_name,
+        endpoint_url=endpoint_url,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+
+    bucket_name = bucket_name
+
+    file = upload_file.file
+    name = upload_file.name
+    file_ext = name.split(".")[-1]
+
+    file_id = str(uuid.uuid4())
+
+    file_path = f"MEDIA/{parent_directory}/{datetime.now().date()}/{file_id}.{file_ext}"
+
+    try:
+        s3.upload_fileobj(file, bucket_name, file_path)
+        file_url = f"{endpoint_url}/{bucket_name}/{file_path}"
+        try:
+            s3.put_object_acl(Bucket=bucket_name, Key=file_path, ACL="public-read")
+            return file_url
+        except Exception as e:
+            print(f"Image setting to public failed: {e}")
+    except Exception as e:
+        print(f"Image upload failed: {e}")
