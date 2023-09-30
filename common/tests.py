313beb8878750c998021b6440e59e03002b5470a
25th-Night <brown_eyed87@naver.com>
2023-09-30 19:18:23 +0900

    Feat) common/tests: s3_file_upload 기능 테스트

---
+++ common/tests.py
@@ -0,0 +1,29 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
+
+from unittest.mock import patch, MagicMock
+
+from .utils import s3_file_upload
+
+
+class FileUploadTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        ...
+
+    @patch("common.utils.client")
+    def test_s3_file_upload(self, client: MagicMock):
+        s3 = MagicMock()
+        client.return_value = s3
+        s3.upload_fileobj.return_value = None
+        s3.put_object_acl.return_value = None
+
+        file = SimpleUploadedFile(
+            "test.txt", b"Test File Data", content_type="text/plain"
+        )
+
+        result = s3_file_upload(file, "files")
+        self.assertTrue(result.startswith("https://kr.object.ncloudstorage.com"))
+
+        s3.upload_fileobj.assert_called_once()
+        s3.put_object_acl.assert_called_once()
