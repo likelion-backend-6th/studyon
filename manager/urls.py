e2f13446cbcd8c3b0236451f4507750c8c0c0830
25th-Night <brown_eyed87@naver.com>
2023-09-30 19:19:21 +0900

    Feat) manager/views: post 생성 FBV 추가

--- manager/urls.py
+++ manager/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
         views.StudyKickoutView.as_view(),
         name="study_kickout",
     ),
-    path("tasks/<int:pk>/", views.PostView.as_view(), name="post_list"),
+    path("tasks/<int:pk>/posts/new/", views.create_post_with_files, name="post_create"),
+    path("tasks/<int:pk>/posts/", views.PostView.as_view(), name="post_list"),
     path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
 ]
