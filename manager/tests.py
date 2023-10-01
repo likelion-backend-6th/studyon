119928ae181de9b3b80a542dadf8818952545ba6
25th-Night <brown_eyed87@naver.com>
2023-10-01 17:38:19 +0900

    Feat) manager/tests: 권한/유효성 검증 테스트 코드 추가

--- manager/tests.py
+++ manager/tests.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from django.forms import ValidationError
 from django.template.response import TemplateResponse
 from django.test import TestCase
 from django.urls import reverse
@@ -8,8 +9,6 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from unittest.mock import patch, MagicMock
 
 from manager.models import File, Post, Study, Task
-
-from recruit.models import Recruit
 
 
 class PostTest(TestCase):
@@ -28,21 +27,18 @@ class PostTest(TestCase):
             info="테스트 인포",
             status=Study.Status.IN_PROGRESS,
         )
-        cls.study.members.add(cls.user02, cls.user03)
+        cls.study.members.add(cls.user01, cls.user02, cls.user03)
 
-        cls.recruit = Recruit.objects.create(
-            study=cls.study,
-            creator=cls.user01,
-            title="테스트 모집공고",
-            deadline=datetime(2023, 8, 31),
-            start=datetime(2023, 9, 1),
-            end=datetime(2023, 10, 31),
-            total_seats=4,
-            target="테스트 타겟",
-            process="테스트 프로세스",
-            info="테스트 인포",
+        cls.study02 = Study.objects.create(
+            creator=cls.user02,
+            title="테스트 모집공고02",
+            start=datetime(2023, 1, 1),
+            end=datetime(2023, 12, 31),
+            process="테스트 프로세스02",
+            info="테스트 인포02",
+            status=Study.Status.IN_PROGRESS,
         )
-        cls.recruit.members.add(cls.user02, cls.user03)
+        cls.study.members.add(cls.user02, cls.user03)
 
         cls.task01 = Task.objects.create(
             study=cls.study,
@@ -70,6 +66,15 @@ class PostTest(TestCase):
             start=datetime(2023, 10, 1),
             end=datetime(2023, 10, 31),
         )
+        cls.task04 = Task.objects.create(
+            study=cls.study02,
+            author=cls.user02,
+            title="테스트 태스크03",
+            description="테스트 설명",
+            start=datetime(2023, 1, 1),
+            end=datetime(2023, 3, 31),
+            is_finished=True,
+        )
 
         cls.post01 = Post.objects.create(
             title="테스트 포스트01",
@@ -88,6 +93,12 @@ class PostTest(TestCase):
             task=cls.task01,
             author=cls.user03,
             content="테스트 게시글 본문03",
+        )
+        cls.post04 = Post.objects.create(
+            title="테스트 포스트04",
+            task=cls.task04,
+            author=cls.user03,
+            content="테스트 게시글 본문04",
         )
 
         cls.file01 = File.objects.create(url="https://www.naver.com")
@@ -132,6 +143,28 @@ class PostTest(TestCase):
             "form-TOTAL_FORMS": 3,
         }
 
+        cls.invalid_post_data = {
+            "title": "새 게시글",
+            "task_id": cls.task01.id,
+            "author": cls.user01,
+            "content": "새 게시글 작성 내용",
+            "form-0-file": SimpleUploadedFile(
+                "test.txt", b"X" * (4 * 1024 * 1024), content_type="text/plain"
+            ),
+            "form-INITIAL_FORMS": 0,
+            "form-TOTAL_FORMS": 1,
+        }
+
+        cls.invalid_modify_data = {
+            "title": "수정 게시글",
+            "content": "수정 게시글 작성 내용",
+            "form-0-file": SimpleUploadedFile(
+                "test.txt", b"X" * (4 * 1024 * 1024), content_type="text/plain"
+            ),
+            "form-INITIAL_FORMS": 0,
+            "form-TOTAL_FORMS": 1,
+        }
+
     def test_post_view(self):
         test_url = reverse("manager:post_list", args=[self.task01.pk])
         self.client.force_login(self.user01)
@@ -141,6 +174,14 @@ class PostTest(TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.context_data["object_list"].count(), 3)
 
+    def test_post_view_permission(self):
+        test_url = reverse("manager:post_list", args=[self.task04.pk])
+        self.client.force_login(self.user01)
+
+        res: TemplateResponse = self.client.get(test_url)
+
+        self.assertEqual(res.status_code, 403)
+
     def test_post_detail(self):
         test_url = reverse("manager:post_detail", args=[self.post01.pk])
         self.client.force_login(self.user01)
@@ -149,6 +190,14 @@ class PostTest(TestCase):
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.context_data["object"].title, self.post01.title)
+
+    def test_post_detail_permission(self):
+        test_url = reverse("manager:post_detail", args=[self.post04.pk])
+        self.client.force_login(self.user01)
+
+        res: TemplateResponse = self.client.get(test_url)
+
+        self.assertEqual(res.status_code, 403)
 
     @patch("common.utils.client")
     def test_post_create(self, client: MagicMock):
@@ -163,7 +212,7 @@ class PostTest(TestCase):
         res: TemplateResponse = self.client.post(test_url, data=self.post_data)
 
         self.assertEqual(res.status_code, 302)
-        self.assertEqual(Post.objects.count(), 4)
+        self.assertEqual(Post.objects.count(), 5)
         self.assertEqual(File.objects.count(), 5)
 
         self.assertTrue(
@@ -172,6 +221,22 @@ class PostTest(TestCase):
 
         self.assertEqual(s3.upload_fileobj.call_count, 3)
         self.assertEqual(s3.put_object_acl.call_count, 3)
+
+    def test_post_create_invalid(self):
+        test_url = reverse("manager:post_create", args=[self.task01.pk])
+        self.client.force_login(self.user01)
+
+        res: TemplateResponse = self.client.post(test_url, data=self.invalid_post_data)
+
+        self.assertRaises(ValidationError)
+
+    def test_post_create_permission(self):
+        test_url = reverse("manager:post_create", args=[self.task04.pk])
+        self.client.force_login(self.user01)
+
+        res: TemplateResponse = self.client.post(test_url, data=self.post_data)
+
+        self.assertEqual(res.status_code, 403)
 
     @patch("common.utils.client")
     def test_post_update(self, client: MagicMock):
@@ -196,6 +261,24 @@ class PostTest(TestCase):
         self.assertEqual(s3.upload_fileobj.call_count, 3)
         self.assertEqual(s3.put_object_acl.call_count, 3)
 
+    def test_post_update_invalid(self):
+        test_url = reverse("manager:post_modify", args=[self.post01.pk])
+        self.client.force_login(self.user01)
+
+        res: TemplateResponse = self.client.post(
+            test_url, data=self.invalid_modify_data
+        )
+
+        self.assertRaises(ValidationError)
+
+    def test_post_update_permission(self):
+        test_url = reverse("manager:post_modify", args=[self.post04.pk])
+        self.client.force_login(self.user01)
+
+        res: TemplateResponse = self.client.post(test_url, data=self.modify_data)
+
+        self.assertEqual(res.status_code, 403)
+
     def test_post_delete(self):
         test_url = reverse("manager:post_delete", args=[self.post01.pk])
         self.client.force_login(self.user01)
@@ -203,6 +286,14 @@ class PostTest(TestCase):
         res: TemplateResponse = self.client.post(test_url)
 
         self.assertEqual(res.status_code, 204)
-        self.assertEqual(Post.objects.count(), 2)
+        self.assertEqual(Post.objects.count(), 3)
         with self.assertRaises(Post.DoesNotExist):
             Post.objects.get(id=self.post01.pk)
+
+    def test_post_delete_permission(self):
+        test_url = reverse("manager:post_delete", args=[self.post04.pk])
+        self.client.force_login(self.user01)
+
+        res: TemplateResponse = self.client.post(test_url)
+
+        self.assertEqual(res.status_code, 403)
