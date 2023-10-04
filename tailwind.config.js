51ac6795652296c52900bd8075c9b45cb2dc63fd
terry <munu220@gmail.com>
2023-10-04 08:15:57 +0000

    Update) custom font in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './templates/**/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Pretendard-Regular', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
   plugins: [],
 }
