d53bc6e6d41df29e70a001fdbfb71450534a8170
terry <munu220@gmail.com>
2023-10-06 04:49:20 +0000

    Fix) 키워드 표시 문제 해결 #167

--- recruit/views.py
+++ recruit/views.py
@@ -44,6 +44,7 @@ class RecruitView(InfiniteListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["tag"] = self.request.GET.get("tag")
+        context["query"] = self.request.GET.get("query")
         context["form"] = SearchForm()
         if self.request.user.is_authenticated:
             context["in_studies"] = Recruit.objects.filter(
