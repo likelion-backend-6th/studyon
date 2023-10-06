7ba03806edbd00703fb31cf31007e6de542f8514
terry <munu220@gmail.com>
2023-10-06 13:44:52 +0000

    Update) templates/studies formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,24 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: [
-    './templates/**/*.html'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Pretendard-Regular', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        'base-black': "#373737",
-      }
+    content: [
+        './templates/**/*.html'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Pretendard-Regular', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'base-black': "#373737",
+                "real-blue": "#0000ff",
+                "wanted-blue": "#6288EF",
+                "wanted-indigo": "#4059E1",
+                "wanted-green": "#89DABF",
+                "wanted-cyan": "#89DADF"
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 
