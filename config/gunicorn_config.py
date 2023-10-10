d7ccfe68915fbd13b440e4bde58e273811d4c1c0
25th-Night <brown_eyed87@naver.com>
2023-10-10 13:06:34 +0900

    Feat) ASGI 앱 상사용에 따른 gunicorn 설정 추가

--- config/gunicorn_config.py
+++ config/gunicorn_config.py
@@ -1,4 +1,15 @@
+import os
 import multiprocessing
+
+import django
+
+ENV = os.getenv("RUN_MODE", "base")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{ENV}")
+
+django.setup()
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
+
+worker_class = "uvicorn.workers.UvicornWorker"
