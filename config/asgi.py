3abaf4a721a49668e006c14072c9685fef420bdb
25th-Night <brown_eyed87@naver.com>
2023-10-10 13:05:55 +0900

    Feat) 설정파일 분기에 따른 asgi 코드 수정

--- config/asgi.py
+++ config/asgi.py
@@ -11,6 +11,21 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-application = get_asgi_application()
+import chat.routing
+
+
+ENV = os.getenv("RUN_MODE", "base")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{ENV}")
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        # "websocket": AuthMiddlewareStack(URLRouter(chat.routing.xx_urlpatterns)),
+    }
+)
