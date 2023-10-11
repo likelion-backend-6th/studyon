adf55923d08486beb51e42fae73046667633432b
Lee816 <lgha0816@naver.com>
2023-10-11 09:28:01 +0900

    Feat) 메세지 앱 뷰 로직 url 연결

---
+++ message/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+app_name = "message"
+urlpatterns = [
+    path("<int:pk>/send/", views.MessageSendView.as_view(), name="send_message"),
+    path("<int:pk>/read/", views.MessageReadView.as_view(), name="read_message"),
+    path("list/", views.MessageListView.as_view(), name="list_messages"),
+    path("notices/", views.NoticeListView.as_view(), name="list_notices"),
+    path(
+        "notice/<int:pk>/delete/",
+        views.NoticeDeleteView.as_view(),
+        name="notice_delete",
+    ),
+]
