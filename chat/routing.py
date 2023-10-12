003f579983df8bc2d9f771cb9f3076bfe6366a3e
25th-Night <brown_eyed87@naver.com>
2023-10-12 14:15:34 +0900

    Fix) black 포매팅 적용

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,8 @@ from django.urls import re_path
 from chat import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/room/(?P<study_id>\w+)/(?P<tags>.+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(
+        r"ws/chat/room/(?P<study_id>\w+)/(?P<tags>.+)/$",
+        consumers.ChatConsumer.as_asgi(),
+    ),
 ]
