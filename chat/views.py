96d36b11e2ed8f45c7effdc39a228e7143195ed9
25th-Night <brown_eyed87@naver.com>
2023-10-12 14:10:19 +0900

    Feat) 태그목록 조회, 채팅방 생성, 채팅방 입장 뷰 생성

--- chat/views.py
+++ chat/views.py
@@ -1,47 +1,58 @@
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import (
-    HttpResponseServerError,
-    HttpResponseNotFound,
-    HttpResponse,
-    HttpResponseRedirect,
-)
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from taggit.utils import parse_tags
+from taggit.models import Tag
 
-from chat.models import Room, Chat
+from chat.forms import TagsForm
+
+
+from chat.models import Room
 from manager.models import Study
 
 
-# Create your views here.
-def study_chat(request, study_id):
-    try:
-        study = Study.objects.get(id=study_id)
-        study_rooms = Room.objects.filter(study=study).count()
+def room_tag_list(request, study_id):
+    study = get_object_or_404(Study, pk=study_id)
+    tag_list = list(Tag.objects.all().values_list("name", flat=True))
+    room_tag_list = list(
+        Room.objects.filter(study=study).values_list("tags__name", flat=True)
+    )
+    return JsonResponse({"tag_list": tag_list, "room_tag_list": room_tag_list})
 
-        if study_rooms >= 3:
-            return HttpResponse("You cannot create more than 3 rooms.")
 
-        if "tags" in request.POST:
-            tags = parse_tags(request.POST["tags"])
+def make_chat_room(request, study_id):
+    tags_form = TagsForm(request.POST)
+    study = get_object_or_404(Study, id=study_id)
 
-            room, created = Room.objects.get_or_create(
-                study=study, defaults={"creator": request.user}
-            )
-            if created:
-                room.tags.add(*tags)
-        else:
-            return HttpResponse("Tags are required to create or join a room.")
+    study_rooms = Room.objects.filter(study=study).count()
 
-        chats = Chat.objects.filter(room=room).order_by("created_at")[:50]
-    except ObjectDoesNotExist:
-        return HttpResponseNotFound("Study not found")
-    except Room.DoesNotExist:
-        return HttpResponseNotFound("Room not found")
-    except Exception as e:
-        return HttpResponseServerError(str(e))
+    if study_rooms >= 3:
+        return HttpResponse("You cannot create more than 3 rooms.")
+
+    if tags_form.is_valid():
+        tag_name = tags_form.data.get("tag_name")
+        tag_id = tags_form.cleaned_data.get("tag_name")
+
+        room, created = Room.objects.get_or_create(
+            study=study, tags__name=tag_name, defaults={"creator": request.user}
+        )
+
+        if created:
+            room.tags.add(tag_name)
+
+        chat_room_url = reverse(
+            "chat:chat_room", kwargs={"study_id": study_id, "tag_id": tag_id}
+        )
+
+        return redirect(chat_room_url)
+
+
+def chat_room(request: HttpRequest, study_id, tag_id):
+    room = get_object_or_404(Room, study_id=study_id, tags__id=tag_id)
+    print(f"room:{room}")
     return render(
         request,
         "chat/room.html",
-        {"study": study, "chats": chats},
+        {
+            "room": room,
+        },
     )
