794a8c0a7536dc0f7cd9a421642bd1c323b4be99
25th-Night <brown_eyed87@naver.com>
2023-10-12 21:56:42 +0900

    Feat) 채팅방 생성, 접속 권한 기능 구현

---
+++ chat/permissions.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from django.http import HttpRequest
+from django.shortcuts import redirect
+from chat.models import Room
+
+from manager.models import Task, Study
+
+
+class ChatRoomMixin(LoginRequiredMixin):
+    def dispatch(self, request: HttpRequest, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+
+        room: Room = self.get_room()
+        if not request.user in room.study.members.all():
+            raise PermissionDenied("Not Authorized to Access")
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_room(self):
+        room_id = self.kwargs["room_id"]
+        room: Room = get_object_or_404(Room, id=room_id)
+        return room
+
+
+def study_access_permission(func):
+    def permission_check(request: HttpRequest, study_id):
+        if request.user.is_anonymous:
+            return redirect("users:login")
+
+        study: Study = get_object_or_404(Study, id=study_id)
+
+        if request.user not in study.members.all():
+            raise PermissionDenied("Not Authorized to Access")
+
+        return func(request, study_id)
+
+    return permission_check
+
+
+def chat_room_access_permission(func):
+    def permission_check(request: HttpRequest, room_id):
+        if request.user.is_anonymous:
+            return redirect("users:login")
+
+        room: Room = get_object_or_404(Task, id=room_id)
+
+        if not request.user in room.study.members.all():
+            raise PermissionDenied("Not Authorized to Access")
+
+        return func(request, room_id)
+
+    return permission_check
