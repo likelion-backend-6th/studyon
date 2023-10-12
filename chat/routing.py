847e7f52fe49085dd1b014ac99c254c2d7dd9c46
Jeong Taek <imjt9345@naver.com>
2023-10-12 10:32:03 +0900

    Update) 채팅에 태그 넣는 기능 추가중

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from chat import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/room/(?P<study_id>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/room/(?P<study_id>\w+)/(?P<tags>.+)/$", consumers.ChatConsumer.as_asgi()),
 ]
