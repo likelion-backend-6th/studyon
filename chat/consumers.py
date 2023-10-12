b732bfa0bc16e6367984f9b7e44ae66d208866f0
25th-Night <brown_eyed87@naver.com>
2023-10-12 22:19:35 +0900

    Feat) ChatConsumer 기본 로직 완성

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,45 +1,91 @@
-import json
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 from channels.db import database_sync_to_async
-from channels.generic.websocket import AsyncWebsocketConsumer
-from django.contrib.auth.models import User
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 from chat.models import Chat, Room
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+    def __init__(self):
+        super().__init__()
+        self.group_name = ""
+        self.room = None
+
     async def connect(self):
-        self.user = self.scope["user"]
-        self.room_name = self.scope["url_route"]["kwargs"]["study_id"]
-        self.room_group_name = f"chat_{self.room_name}"
+        user = self.scope["user"]
+        room_id = self.scope["url_route"]["kwargs"]["room_id"]
 
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        if not user.is_authenticated:
+            self.close()
 
-        await self.accept()
+        try:
+            self.room = await self.get_room_by_room_id(room_id)
+            study_members = await self.get_study_members_by_room(self.room)
+            study_member_list = study_members
+            if user not in study_member_list:
+                self.close()
+        except Room.DoesNotExist:
+            self.close()
+        else:
+            self.group_name = self.room.chat_group_name
+
+            await self.channel_layer.group_add(self.group_name, self.channel_name)
+
+            await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        if self.group_name:
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        room_id = self.scope["url_route"]["kwargs"]["study_id"]
-        username = self.scope["user"].username
-        await self.save_message(username=username, room_id=room_id, content=message)
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "chat_message",
+    async def receive_json(self, content):
+        user = self.scope["user"]
+        _type = content["type"]
+
+        if _type == "chat.message":
+            sender = user.username
+            message = content["message"]
+            room_id = self.scope["url_route"]["kwargs"]["room_id"]
+            chat = await self.save_message(
+                username=sender, room_id=room_id, content=message
+            )
+            message_dict = {
+                "type": "chat.message",
                 "message": message,
-                "user": username,
-            },
-        )
+                "sender": sender,
+                "datetime": chat.created_at.strftime("%Y-%m-%dT%H:%M:%S"),
+            }
+            await self.channel_layer.group_send(
+                self.group_name,
+                message_dict,
+            )
+        else:
+            print(f"Invalid message type : {_type}")
 
-    async def chat_message(self, event):
-        await self.send(text_data=json.dumps(event))
+    async def chat_message(self, message_dict):
+        await self.send_json(
+            {
+                "type": "chat.message",
+                "message": message_dict["message"],
+                "sender": message_dict["sender"],
+                "datetime": message_dict["datetime"],
+            }
+        )
 
     @database_sync_to_async
     def save_message(self, username, room_id, content):
         user = User.objects.get(username=username)
-        room = Room.objects.filter(study__id=room_id).order_by("created_at").first()
-        Chat.objects.create(creator=user, room=room, content=content)
+        room = Room.objects.get(id=room_id)
+        chat = Chat.objects.create(creator=user, room=room, content=content)
+        return chat
+
+    @database_sync_to_async
+    def get_room_by_room_id(self, room_id):
+        room = get_object_or_404(Room, id=room_id)
+        return room
+
+    @database_sync_to_async
+    def get_study_members_by_room(self, room):
+        study_members = room.study.members.all()
+        return study_members
