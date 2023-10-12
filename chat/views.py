0e2605eec9471bd065ca449841941b03625b9068
25th-Night <brown_eyed87@naver.com>
2023-10-12 22:01:13 +0900

    Feat) 뷰에 권한 로직 추가, 채팅룸 뷰에 페이지네이션 추가

--- chat/views.py
+++ chat/views.py
@@ -1,15 +1,23 @@
 from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
+from django.views.generic import ListView
+
 from taggit.models import Tag
 
 from chat.forms import TagsForm
 
 
-from chat.models import Room
+from chat.models import Chat, Room
+from chat.permissions import (
+    ChatRoomMixin,
+    chat_room_access_permission,
+    study_access_permission,
+)
 from manager.models import Study
 
 
+@study_access_permission
 def room_tag_list(request, study_id):
     study = get_object_or_404(Study, pk=study_id)
     tag_list = list(Tag.objects.all().values_list("name", flat=True))
@@ -19,6 +27,7 @@ def room_tag_list(request, study_id):
     return JsonResponse({"tag_list": tag_list, "room_tag_list": room_tag_list})
 
 
+@study_access_permission
 def make_chat_room(request, study_id):
     tags_form = TagsForm(request.POST)
     study = get_object_or_404(Study, id=study_id)
@@ -29,8 +38,7 @@ def make_chat_room(request, study_id):
         return HttpResponse("You cannot create more than 3 rooms.")
 
     if tags_form.is_valid():
-        tag_name = tags_form.data.get("tag_name")
-        tag_id = tags_form.cleaned_data.get("tag_name")
+        tag_name = tags_form.cleaned_data.get("tag_name")
 
         room, created = Room.objects.get_or_create(
             study=study, tags__name=tag_name, defaults={"creator": request.user}
@@ -39,20 +47,41 @@ def make_chat_room(request, study_id):
         if created:
             room.tags.add(tag_name)
 
-        chat_room_url = reverse(
-            "chat:chat_room", kwargs={"study_id": study_id, "tag_id": tag_id}
-        )
+        chat_room_url = reverse("chat:chat_room", kwargs={"room_id": room.id})
 
         return redirect(chat_room_url)
 
 
-def chat_room(request: HttpRequest, study_id, tag_id):
-    room = get_object_or_404(Room, study_id=study_id, tags__id=tag_id)
-    print(f"room:{room}")
+@chat_room_access_permission
+def chat_room(request: HttpRequest, room_id):
+    room = get_object_or_404(Room.objects.select_related("study"), id=room_id)
+    chats = Chat.objects.filter(room=room)
     return render(
         request,
         "chat/room.html",
         {
             "room": room,
+            "chats": chats,
         },
     )
+
+
+class ChatRoomView(ChatRoomMixin, ListView):
+    model = Chat
+    template_name = "chat/room.html"
+    context_object_name = "chats"
+    paginate_by = 30
+
+    def get_queryset(self):
+        room_id = self.kwargs["room_id"]
+        room = get_object_or_404(Room.objects.select_related("study"), id=room_id)
+        queryset = Chat.objects.filter(room=room)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        room_id = self.kwargs["room_id"]
+        room = get_object_or_404(Room.objects.select_related("study"), id=room_id)
+        context["room"] = room
+        return context
