caa2406433e1d31a1460c44d976358eaf298e2e5
25th-Night <brown_eyed87@naver.com>
2023-10-12 14:10:00 +0900

    Fix) 태그 입력 Form 수정

--- chat/forms.py
+++ chat/forms.py
@@ -1,9 +1,22 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+from taggit.models import Tag
 
 
 class TagsForm(forms.Form):
-    tags = forms.CharField(
+    tag_name = forms.CharField(
         widget=forms.TextInput(
             attrs={"class": "my-custom-class", "placeholder": "Enter tags..."}
         )
     )
+
+    def clean_tag_name(self):
+        tag_name = self.cleaned_data.get("tag_name")
+        try:
+            tag = Tag.objects.get(name=tag_name)
+        except Tag.DoesNotExist:
+            raise ValidationError("Tag does not exist")
+
+        tag_id = tag.id
+        return tag_id
