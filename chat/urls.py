79d14efd2e86569c150ec22943cf70a653fbd80e
25th-Night <brown_eyed87@naver.com>
2023-10-12 14:10:28 +0900

    Feat) 생성한 뷰의 url 등록

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,7 @@ from chat import views
 app_name = "chat"
 
 urlpatterns = [
-    path("room/<int:study_id>/", views.study_chat, name="study_chat_room"),
+    path("room/<int:study_id>/", views.make_chat_room, name="make_chat_room"),
+    path("room/<int:study_id>/<int:tag_id>/", views.chat_room, name="chat_room"),
+    path("room/<int:study_id>/tags/", views.room_tag_list, name="room_tag_list"),
 ]
