eeb0d9b81955448000834168f1c367320559497f
25th-Night <brown_eyed87@naver.com>
2023-10-15 10:09:36 +0900

    Feat) 생성한 task들을 beat_schedule에 등록

---
+++ config/celery.py
@@ -0,0 +1,42 @@
+import os
+
+from celery import Celery
+from celery.schedules import crontab
+
+
+ENV = os.getenv("RUN_MODE", "base")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"config.settings.{ENV}")
+
+app = Celery("config")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "clean_up_old_rooms": {
+        "task": "chat.tasks.cleanup_old_rooms",
+        "schedule": crontab(hour=0, minute=3),
+    },
+    "clean_up_old_studies": {
+        "task": "manager.tasks.cleanup_old_studies",
+        "schedule": crontab(hour=0, minute=4),
+    },
+    "clean_up_old_tasks": {
+        "task": "manager.tasks.cleanup_old_tasks",
+        "schedule": crontab(hour=0, minute=5),
+    },
+    "clean_up_old_posts": {
+        "task": "manager.tasks.cleanup_old_posts",
+        "schedule": crontab(hour=0, minute=6),
+    },
+    "clean_up_old_messagges": {
+        "task": "message.tasks.cleanup_old_messagges",
+        "schedule": crontab(hour=0, minute=7),
+    },
+    "clean_up_old_recruits": {
+        "task": "recruit.tasks.cleanup_old_recruits",
+        "schedule": crontab(hour=0, minute=8),
+    },
+}
