b42423d4ad67cf809018351e4d01f6d164ca7d3c
25th-Night <brown_eyed87@naver.com>
2023-10-17 03:44:37 +0900

    Update) 테스트용 파일 작성 및 테스트 진행

--- chat/management/commands/query.py
+++ chat/management/commands/query.py
@@ -2,7 +2,7 @@ from random import *
 
 from django.core.management.base import BaseCommand
 
-from common.utils import filter_model_data_to_delete
+from common.utils import filter_model_data_to_change_status, filter_model_data_to_delete
 
 
 # TODO: 커맨드 활용
@@ -12,7 +12,20 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         rooms = filter_model_data_to_delete(
-            "chat", "Room", "closed_at", "closed_at", None, False
+            "chat", "Room", "closed_at", 30, "closed_at", None, False
         )
         for room in rooms:
             print(room)
+
+        tasks_to_delete = filter_model_data_to_delete(
+            "manager", "Task", "updated_at", 30, "is_active", False, True
+        )
+        for task in tasks_to_delete:
+            print(task)
+
+        studies_to_change = filter_model_data_to_change_status(
+            "manager", "Study", "end"
+        )
+
+        for study in studies_to_change:
+            print(study)
