c450c8424f15f3eb223a77570a388d7b6737eada
Lee816 <lgha0816@naver.com>
2023-10-17 16:10:04 +0900

    Update) 채팅방 입장,퇴장 알림 추가 및 알림유무 표시 에러 수정

--- message/context_processors.py
+++ message/context_processors.py
@@ -1,11 +1,11 @@
 from .models import Message, Notice
 
 
-def get_recent_message(self):
-    recent_message = Message.objects.first()
+def get_recent_message(request):
+    recent_message = Message.objects.filter(reciever=request.user).first()
     return {"recent_message": recent_message}
 
 
-def get_recent_notice(self):
-    recent_notice = Notice.objects.first()
+def get_recent_notice(request):
+    recent_notice = Notice.objects.filter(user=request.user).first()
     return {"recent_notice": recent_notice}
