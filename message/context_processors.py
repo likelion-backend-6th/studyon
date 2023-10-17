508337eb27178d6c6bafeac49d8f69c0142277a2
Lee816 <lgha0816@naver.com>
2023-10-17 17:16:31 +0900

    Update) 접속시 비로그인으로 인한 context 에러 수정

--- message/context_processors.py
+++ message/context_processors.py
@@ -2,10 +2,18 @@ from .models import Message, Notice
 
 
 def get_recent_message(request):
-    recent_message = Message.objects.filter(reciever=request.user).first()
-    return {"recent_message": recent_message}
+    if request.user.is_authenticated:
+        recent_message = Message.objects.filter(reciever=request.user).first()
+        return {"recent_message": recent_message}
+    else:
+        recent_message = None
+        return {"recent_message": recent_message}
 
 
 def get_recent_notice(request):
-    recent_notice = Notice.objects.filter(user=request.user).first()
-    return {"recent_notice": recent_notice}
+    if request.user.is_authenticated:
+        recent_notice = Notice.objects.filter(user=request.user).first()
+        return {"recent_notice": recent_notice}
+    else:
+        recent_notice = None
+        return {"recent_notice": recent_notice}
