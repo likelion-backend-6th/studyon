06e095b716436e2fa755d3b7e09645cea279897f
Jeong Taek <imjt9345@naver.com>
2023-10-17 17:48:18 +0900

    Update) 모집글 목록에 is_active가 True인 모집글만 출력

--- recruit/views.py
+++ recruit/views.py
@@ -47,7 +47,9 @@ class RecruitView(InfiniteListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["recruits_list"] = Recruit.objects.all().order_by("-created_at")
+        context["recruits_list"] = Recruit.objects.filter(is_active=True).order_by(
+            "-created_at"
+        )
         context["tag"] = self.request.GET.get("tag")
         context["query"] = self.request.GET.get("query")
         context["form"] = SearchForm()
