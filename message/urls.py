fd0416e33736a5fae81c41c90d2bbf5c735ef794
25th-Night <brown_eyed87@naver.com>
2023-10-19 21:52:57 +0900

    Feat) view 수정, 추가에 따른 url 수정, 추가

--- message/urls.py
+++ message/urls.py
@@ -5,11 +5,12 @@ from . import views
 app_name = "message"
 urlpatterns = [
     path(
-        "<int:study_id>/send/<int:user_id>",
-        views.MessageSendView.as_view(),
-        name="send_message",
+        "send/<int:user_id>/",
+        views.MessageSendTargetView.as_view(),
+        name="send_message_target",
     ),
     path("<int:pk>/read/", views.MessageReadView.as_view(), name="read_message"),
+    path("<int:pk>/delete/", views.MessageDeleteView.as_view(), name="delete_message"),
     path("list/", views.MessageListView.as_view(), name="list_messages"),
     path("notices/", views.NoticeListView.as_view(), name="list_notices"),
     path(
