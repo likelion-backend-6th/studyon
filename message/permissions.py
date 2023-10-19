41bd27a83e5dc40004eba48e1bd60a409d9be148
25th-Night <brown_eyed87@naver.com>
2023-10-19 21:36:37 +0900

    Feat) 알림, 메시지 관련 권한 체크 함수 추가

--- message/permissions.py
+++ message/permissions.py
@@ -4,9 +4,26 @@ from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
 from django.http import HttpRequest
 from manager.models import Study
+from message.models import Message, Notice
 
 
-class MessageAccessMixin(LoginRequiredMixin):
+class NoticeAccessMixin(LoginRequiredMixin):
+    def dispatch(self, request: HttpRequest, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+
+        notice: Notice = self.get_notice()
+        if request.user != notice.user:
+            raise PermissionDenied("Not Authorized to Access")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_notice(self):
+        notice_id = self.kwargs["pk"]
+        notice: Notice = get_object_or_404(User, id=notice_id)
+        return notice
+
+
+class MessageSendMixin(LoginRequiredMixin):
     def dispatch(self, request: HttpRequest, *args, **kwargs):
         if not request.user.is_authenticated:
             return self.handle_no_permission()
@@ -18,6 +35,22 @@ class MessageAccessMixin(LoginRequiredMixin):
         return super().dispatch(request, *args, **kwargs)
 
     def get_receiver(self):
-        recruit_id = self.kwargs["user_id"]
-        receiver: User = get_object_or_404(User, id=recruit_id)
+        receiver_id = self.kwargs["user_id"]
+        receiver: User = get_object_or_404(User, id=receiver_id)
         return receiver
+
+
+class MessageAccessMixin(LoginRequiredMixin):
+    def dispatch(self, request: HttpRequest, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+
+        message: Message = self.get_message()
+        if request.user != message.reciever:
+            raise PermissionDenied("Not Authorized to Access")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_message(self):
+        pk = self.kwargs["pk"]
+        message: Message = get_object_or_404(Message, id=pk)
+        return message
