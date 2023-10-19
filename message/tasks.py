29192867fd661ba6e81a98840324a59f8fe86271
25th-Night <brown_eyed87@naver.com>
2023-10-19 21:54:50 +0900

    Feat) 수신 30일 지난 알림 삭제 task 추가

--- message/tasks.py
+++ message/tasks.py
@@ -9,3 +9,11 @@ def cleanup_old_messages():
         "message", "Message", "read_at", 30, "read_at", None, False
     )
     messages_to_delete.delete()
+
+
+@shared_task
+def cleanup_old_notices():
+    notices_to_delete = filter_model_data_to_delete(
+        "message", "Message", "created_at", 30, "created_at", None, False
+    )
+    notices_to_delete.delete()
