a210954432117c28be0c297e3f84ab9a0ffd431e
Lee816 <lgha0816@naver.com>
2023-10-23 14:29:40 +0900

    Update) url 에서 끝에 / 빠진것 추가

--- manager/urls.py
+++ manager/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("<int:pk>/", views.StudyDetailView.as_view(), name="study_detail"),
     path("<int:pk>/like/", views.studies_like_recruit, name="studies_like_recruit"),
     path(
-        "<int:pk>/unlike", views.studies_unlike_recruit, name="studies_unlike_recruit"
+        "<int:pk>/unlike/", views.studies_unlike_recruit, name="studies_unlike_recruit"
     ),
     path("<int:pk>/modify/", views.StudyModifyView.as_view(), name="study_modify"),
     path(
